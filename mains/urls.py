fd2502e7824b3f89e7ac29dae5ed5a09f1cab96a
이찬우 <overelectric@gmail.com>
2020-06-30 15:03:24 +0900

    temp

--- mains/urls.py
+++ mains/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('content/', views.content, name='content'),
     path('content1/', views.content1, name='content1'),
 
-
-    
+    path('insite/', views.insite, name='insite'),
     # DB 생성용
     path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
     path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
