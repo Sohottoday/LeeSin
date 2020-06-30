c49d09d0f2a81075d27e3b92513edc7d317189fe
이찬우 <overelectric@gmail.com>
2020-06-30 14:41:29 +0900

    merge

--- mains/models.py
+++ mains/models.py
@@ -58,3 +58,65 @@ class Company(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     # 게시한 공고
     posted_recruit = models.ManyToManyField(Recruit, related_name='posting_company')
+
+
+
+
+
+# Create your models here.
+class CountIssue(models.Model):
+    javascript = models.CharField(max_length=20)
+    java = models.CharField(max_length=20)
+    python = models.CharField(max_length=20)
+    c = models.CharField(max_length=20)
+    csharp = models.CharField(max_length=20)
+    cplus = models.CharField(max_length=20)
+    go = models.CharField(max_length=20)
+    ruby = models.CharField(max_length=20)
+    typescript = models.CharField(max_length=20)
+    php = models.CharField(max_length=20)
+    scala = models.CharField(max_length=20)
+    rust = models.CharField(max_length=20)
+    kotlin = models.CharField(max_length=20)
+    swift = models.CharField(max_length=20)
+    shell = models.CharField(max_length=20)
+    date = models.DateField(auto_now=True)
+
+
+class CountCommit(models.Model):
+    javascript = models.CharField(max_length=20)
+    java = models.CharField(max_length=20)
+    python = models.CharField(max_length=20)
+    c = models.CharField(max_length=20)
+    csharp = models.CharField(max_length=20)
+    cplus = models.CharField(max_length=20)
+    go = models.CharField(max_length=20)
+    ruby = models.CharField(max_length=20)
+    typescript = models.CharField(max_length=20)
+    php = models.CharField(max_length=20)
+    scala = models.CharField(max_length=20)
+    rust = models.CharField(max_length=20)
+    kotlin = models.CharField(max_length=20)
+    swift = models.CharField(max_length=20)
+    shell = models.CharField(max_length=20)
+    date = models.DateField()
+
+
+class CountRepository(models.Model):
+    javascript = models.CharField(max_length=20)
+    java = models.CharField(max_length=20)
+    python = models.CharField(max_length=20)
+    c = models.CharField(max_length=20)
+    csharp = models.CharField(max_length=20)
+    cplus = models.CharField(max_length=20)
+    go = models.CharField(max_length=20)
+    ruby = models.CharField(max_length=20)
+    typescript = models.CharField(max_length=20)
+    php = models.CharField(max_length=20)
+    scala = models.CharField(max_length=20)
+    rust = models.CharField(max_length=20)
+    kotlin = models.CharField(max_length=20)
+    swift = models.CharField(max_length=20)
+    shell = models.CharField(max_length=20)
+    date = models.DateField()
+
--- mains/urls.py
+++ mains/urls.py
@@ -8,4 +8,25 @@ urlpatterns = [
     path('rank/', views.rank, name='rank'),
     path('content/', views.content, name='content'),
     path('content1/', views.content1, name='content1'),
+
+
+    
+    # DB 생성용
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    path('issue/javascript/', views.issuejavascript, name='issuejavascript'),
+    
+    # path('content1/', views.content1, name='content1'),
 ]
